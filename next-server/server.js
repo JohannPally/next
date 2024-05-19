c1b027eabe34b70dea86ff1846cbae727db5bab1
Johann Pally <pallyjohann@gmail.com>
2024-05-18 23:11:12 -0500

    using react socket tutorial

--- next-server/server.js
+++ next-server/server.js
@@ -1,25 +1,37 @@
-const express = require('express');
-const cors = require('cors')({
-    origin: 'http://localhost:3000'
-});
-const http = require('http');
+// const express = require('express');
+// const cors = require('cors');
+// const http = require('http');
 
-const app = express();
-app.use(cors());
+// const app = express();
+// app.use(cors({ origin: '*' }));
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
+// const { Server } = require("socket.io");
+// const io = new Server(server);
+
+
+// app.get('/', (req, res) => {
+//     res.send('Request Received.');
+// });
+
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+// });
+
+// server.listen(3001, () => {
+//     console.log('listening on *:3001');
+// });
+
 const { Server } = require("socket.io");
-const io = new Server(server);
 
-
-app.get('/', (req, res) => {
-    res.send('Request Received.');
+const io = new Server({
+    cors: {
+        origin: "http://localhost:3000"
+    }
 });
+  
+io.listen(4000);
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-});
-
-server.listen(3001, () => {
-    console.log('listening on *:3001');
 });
